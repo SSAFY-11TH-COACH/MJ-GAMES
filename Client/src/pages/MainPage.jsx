4ce88a8c97e6b9b27c5fe3b7a6472ddf13e12a5a
서지수 <diduedidue@naver.com>
2024-11-12 14:21:26 +0900

    Feat: 메인 페이지 onClose 변수 전달 설정 및 초안 마무리

--- Client/src/pages/MainPage.jsx
+++ Client/src/pages/MainPage.jsx
@@ -7,7 +7,7 @@ export default function MainPage() {
     const [showJoinModal, setShowJoinModal] = useState(false)
 
     return (
-        <div className="flex flex-col items-center space-y-8 p-12">
+        <div className="flex flex-col items-center mt-24 space-y-8 p-12">
             <button 
                 onClick={() => setShowCreateModal(true)} 
                 className="bg-gray-300 text-black text-4xl font-bold py-8 px-16 rounded-xl shadow-2xl w-full max-w-2xl"
@@ -21,8 +21,12 @@ export default function MainPage() {
                 이벤트 참여
             </button>
 
-            {showCreateModal && <EventCreateModal />}
-            {showJoinModal && <EventJoinModal />}
+            {showCreateModal && (
+                <EventCreateModal onClose={() => setShowCreateModal(false)} />
+            )}
+            {showJoinModal && (
+                <EventJoinModal onClose={() => setShowJoinModal(false)} />
+            )}
         </div>
     )
 }
