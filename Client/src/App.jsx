9a96b0be00c46ba665ab7e7150c544409880e77a
서지수 <diduedidue@naver.com>
2024-11-13 10:14:28 +0900

    Chore: martine 라이브러리 설치 및 적용

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import MainPage from './pages/MainPage'
-import EventDetailPage from './pages/EventDetailPage'
-import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import MainPage from './pages/MainPage';
+import EventDetailPage from './pages/EventDetailPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <div className="w-full min-h-screen bg-gray-800">
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/event/:eventId" element={<EventDetailPage />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <div className="w-full min-h-screen bg-gray-800">
+          <Header />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/event/:eventId" element={<EventDetailPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
