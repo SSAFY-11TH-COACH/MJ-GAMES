6a28f81c62b39c6d0b134ce6e5eba9032f83d71e
서지수 <diduedidue@naver.com>
2024-11-13 09:38:45 +0900

    Style: 폰트 및 로고 수정

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -4,11 +4,11 @@ import githubIcon from '../assets/images/github-mark.svg'
 
 export default function Header() {
     return (
-        <header className="w-full bg-gray-800 text-white p-4 shadow-md flex justify-center">
-            <div className="container max-w-8xl flex justify-between items-center">
+        <header className="w-full bg-gray-800 text-white p-4 border-b border-gray-700">
+            <div className="container max-w-7xl flex justify-between items-center mx-auto">
                 <Link to="/" className="flex items-center">
-                    <img src={logo} alt="Logo" className="h-12 mr-3" /> {/* 로고 이미지 */}
-                    {/*<h1 className="text-xl text-sky-200 font-bold">GAMES</h1>  필요시 로고 옆에 텍스트 추가 */}
+                    <img src={logo} alt="Logo" className="h-10 mr-2" /> {/* 로고 이미지 */}
+                    <h1 className="text-xl font-sans text-sky-200 font-normal">게임즈</h1> {/* 필요시 로고 옆에 텍스트 추가 */}
                 </Link>
                 <nav className="flex space-x-2 font-geist font-light">
                     <a href="https://github.com/thingseong" target="_blank" rel="noopener noreferrer" className="flex items-center px-4">
